ccf3846d2c586663229433904dda315491c128da
r0ckwav3 <peterrockwave@gmail.com>
2022-12-20 17:34:14 -1000

    calculate indexmatches

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,7 @@ fn chartodigit(c: char) -> u64{
     }
 }
 
-pub fn wordtonum(word: &String) -> u64{
+pub fn wordtonum(word: &str) -> u64{
     let mut ans = 0;
     for (i,c) in word.chars().enumerate(){
         ans += chartodigit(c) * 10_u64.pow((word.len()-(i+1)) as u32);
@@ -35,10 +35,32 @@ pub fn wordtonum(word: &String) -> u64{
     ans
 }
 
-pub fn numtophrase<'a, Itertype>(num: u64, words: &Itertype) -> Vec<&'a str>
+fn countdigits(num: u64) -> u32{
+   ((num as f64).log(10.0).floor() as u32) - 1
+}
+
+// think of it as a slice [start, end)
+fn truncatenum(num: u64, start: u32, end: u32) -> u64{
+    let numlen = countdigits(num);
+    (num % 10_u64.pow(numlen-start)) / 10_u64.pow(numlen-end)
+}
+
+pub fn numtophrase<'a, Itertype>(num: u64, words: &mut Itertype) -> Vec<&'a str>
 where
     Itertype: iter::Iterator<Item = &'a str>
 {
+    let numlen = countdigits(num);
+    // if a word can be inserted at index i and match all the following numbers
+    let mut indexmatches: Vec<Vec<&str>> = Vec::with_capacity(numlen as usize);
+    for word in words{
+        let wordnum = wordtonum(word);
+        for index in 0..(numlen - ((word.len() as u32) - 1)){
+            if wordnum == truncatenum(num, index, index + word.len() as u32){
+                indexmatches[index as usize].push(word);
+            }
+        }
+    }
+    
     vec!["test phrase"]
 }
 
@@ -56,8 +78,8 @@ mod tests {
     
     #[test]
     fn wordtonum_test() {
-        assert_eq!(wordtonum(&String::from("test")), 8378);
-        assert_eq!(wordtonum(&String::from("a")), 2);
+        assert_eq!(wordtonum("test"), 8378);
+        assert_eq!(wordtonum("a"), 2);
         wordtonum(&String::from("longwordtest"));
     }
     
