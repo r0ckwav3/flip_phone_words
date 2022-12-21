426e64b3f3a3bcd2c56050858c7c3ca85dee4c63
r0ckwav3 <peterrockwave@gmail.com>
2022-12-20 17:35:41 -1000

    made words mut, reformatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,20 @@
 use std::iter;
 
-pub fn modtest(){
+pub fn modtest() {
     println!("test");
 }
 
-pub fn isprime(n: u64) -> bool{
-    for i in 2..n{
-        if n%i == 0 {
+pub fn isprime(n: u64) -> bool {
+    for i in 2..n {
+        if n % i == 0 {
             return false;
         }
     }
     true
 }
 
-fn chartodigit(c: char) -> u64{
-    match c{
+fn chartodigit(c: char) -> u64 {
+    match c {
         'a'..='c' => 2,
         'd'..='f' => 3,
         'g'..='i' => 4,
@@ -23,51 +23,51 @@ fn chartodigit(c: char) -> u64{
         'p'..='s' => 7,
         't'..='v' => 8,
         'w'..='z' => 9,
-        _other => 0
+        _other => 0,
     }
 }
 
-pub fn wordtonum(word: &str) -> u64{
+pub fn wordtonum(word: &str) -> u64 {
     let mut ans = 0;
-    for (i,c) in word.chars().enumerate(){
-        ans += chartodigit(c) * 10_u64.pow((word.len()-(i+1)) as u32);
+    for (i, c) in word.chars().enumerate() {
+        ans += chartodigit(c) * 10_u64.pow((word.len() - (i + 1)) as u32);
     }
     ans
 }
 
-fn countdigits(num: u64) -> u32{
-   ((num as f64).log(10.0).floor() as u32) - 1
+fn countdigits(num: u64) -> u32 {
+    ((num as f64).log(10.0).floor() as u32) - 1
 }
 
 // think of it as a slice [start, end)
-fn truncatenum(num: u64, start: u32, end: u32) -> u64{
+fn truncatenum(num: u64, start: u32, end: u32) -> u64 {
     let numlen = countdigits(num);
-    (num % 10_u64.pow(numlen-start)) / 10_u64.pow(numlen-end)
+    (num % 10_u64.pow(numlen - start)) / 10_u64.pow(numlen - end)
 }
 
 pub fn numtophrase<'a, Itertype>(num: u64, words: &mut Itertype) -> Vec<&'a str>
 where
-    Itertype: iter::Iterator<Item = &'a str>
+    Itertype: iter::Iterator<Item = &'a str>,
 {
     let numlen = countdigits(num);
     // if a word can be inserted at index i and match all the following numbers
     let mut indexmatches: Vec<Vec<&str>> = Vec::with_capacity(numlen as usize);
-    for word in words{
+    for word in words {
         let wordnum = wordtonum(word);
-        for index in 0..(numlen - ((word.len() as u32) - 1)){
-            if wordnum == truncatenum(num, index, index + word.len() as u32){
+        for index in 0..(numlen - ((word.len() as u32) - 1)) {
+            if wordnum == truncatenum(num, index, index + word.len() as u32) {
                 indexmatches[index as usize].push(word);
             }
         }
     }
-    
+
     vec!["test phrase"]
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
     #[test]
     fn chartodigit_test() {
         assert_eq!(chartodigit('a'), 2);
@@ -75,14 +75,14 @@ mod tests {
         assert_eq!(chartodigit('u'), 8);
         assert_eq!(chartodigit(' '), 0);
     }
-    
+
     #[test]
     fn wordtonum_test() {
         assert_eq!(wordtonum("test"), 8378);
         assert_eq!(wordtonum("a"), 2);
         wordtonum(&String::from("longwordtest"));
     }
-    
+
     #[test]
     fn isprime_test() {
         assert!(!isprime(91));
--- src/main.rs
+++ src/main.rs
@@ -8,40 +8,35 @@ fn main() {
     let word = String::from("prime");
     let wordnum = wordtonum(&word);
     println!("\"{}\" on a keypad is: {}", word, wordnum);
-    if isprime(wordnum){
+    if isprime(wordnum) {
         println!("prime!");
-    }else{
+    } else {
         println!("not prime ");
     }
-    
-    for prefix in 0..100{
-        let tempnum = prefix*100000 + wordnum;
-        if isprime(tempnum){
+
+    for prefix in 0..100 {
+        let tempnum = prefix * 100000 + wordnum;
+        if isprime(tempnum) {
             println!("... but {} is!", tempnum);
         }
     }
-    
+
     // num to word stuff
     let filename = "top1000words.txt";
     let filecontents = fs::read_to_string(filename)
-        .expect(&format!("could not find file \"{}\"", filename));
-    let words = filecontents.split_whitespace();
-    
-    
+            .expect(&format!("could not find file \"{}\"", filename));
+    let mut words = filecontents.split_whitespace();
+
     println!("Please input a number:");
     let mut input = String::new();
     io::stdin()
         .read_line(&mut input)
         .expect("Failed to read line");
-    
-    let num = input
-        .trim()
-        .parse()
-        .expect("Input is not a number");
-    
-    
-    let result = numtophrase(num, &words);
-    for phrase in result{
+
+    let num = input.trim().parse().expect("Input is not a number");
+
+    let result = numtophrase(num, &mut words);
+    for phrase in result {
         println!("{}", phrase)
     }
 }
